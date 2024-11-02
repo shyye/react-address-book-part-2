6961f427b4e80c8a49094bc83ac20d3f8bc23337
shyye <emma.art.dev@gmail.com>
2024-11-02 16:33:06 +0100

    Add list elements to Dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,7 +1,21 @@
+import { useContext } from "react";
+import { AppContext } from "../../App";
+import { Link } from "react-router-dom";
+
 function Dashboard() {
+  const { contacts } = useContext(AppContext);
+
+
     return (
       <div>
         <h1>Dashboard</h1>
+        <ul>
+          {contacts.map((contact) => (
+            <li key={contact.id}>
+              <Link to={`/contact/${contact.id}`}>{contact.firstName} {contact.lastName}</Link>
+            </li>
+          ))}
+        </ul>
       </div>
     );
       
