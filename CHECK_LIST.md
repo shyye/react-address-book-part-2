2d5c3728582d980452f03e0b08990d93d67c2df3
shyye <emma.art.dev@gmail.com>
2024-11-02 16:12:54 +0100

    Add checklist and part 1 notes

--- CHECK_LIST.md
+++ CHECK_LIST.md
@@ -1,5 +1,31 @@
 # Checklist
 
+## Part 1
+### State Model
+```
+[
+    {
+        id: 1,
+        firstname: "First Name",
+        lastname: "Last Name",
+        street: "Street 1",
+        city: "City 1"
+    }
+]
+```
+
+### Component tree
+```mermaid
+flowchart TD
+    A[App] --> B(Pages)
+    B --> C[Dashboard]
+    B --> D[ContactView]
+    C --> E(ContactList)
+    C --> F(ContactListItem)
+    D --> G(Create)
+    D --> H(View)
+```
+
 ## Core
 
 - [ ] 1. A user can view a dashboard that displays a list of contacts
--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,25 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import ContactView from "./pages/ContactView/ContactView";
+import { createContext, useEffect, useState } from "react";
+
+const AppContext = createContext();
 
 function App() {
+  const [contacts, setContacts] = useState([]);
+
+  // Get data
+  const getData = async () => {
+    const res = await fetch("https://boolean-uk-api-server.fly.dev/shyye/contact");
+    const data = await res.json();
+    setContacts(data);
+  };
+
+  // Load data
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <>
       <header>
@@ -15,11 +32,13 @@ function App() {
           </ul>
         </nav>
       </header>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/add" element={<ContactView />} />
-        <Route path="/contact/:id" element={<ContactView />} />     
-      </Routes>
+      <AppContext.Provider value={{ contacts, setContacts }}>
+        <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/add" element={<ContactView />} />
+            <Route path="/contact/:id" element={<ContactView />} />
+        </Routes>
+      </AppContext.Provider>
     </>
   );
 }
